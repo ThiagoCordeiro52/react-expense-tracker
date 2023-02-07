09eac6b4b5c701fc45759a107d43d613d87bfa12
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-02-07 08:53:03 -0300

    :sparkles: feat: initial files created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Body, Container, Header, HeaderText } from './styles';
+
+function App() {
+  return (
+    <Container>
+      <Header>
+        <HeaderText>Financial System</HeaderText>
+      </Header>
+      <Body>...</Body>
+    </Container>
+  );
+}
+
+export default App;
